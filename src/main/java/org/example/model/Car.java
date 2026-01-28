c5c1582213ff54c8c334bbbb5746e8e02a3628c2
Mihaela Catan <mihaelacatan7@gmail.com>
2026-01-28 10:22:40 +0200

    Add Car method to check validity of a car

--- src/main/java/org/example/model/Car.java
+++ src/main/java/org/example/model/Car.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.commons.csv.CSVRecord;
 import org.example.parser.CarCsvProcessor;
 import org.example.scraper.CarDetailsPage;
@@ -17,6 +18,14 @@ public record Car(String link, String name, String model, String generation,
     private static final String CSV_YEAR = "Year";
     private static final String CSV_MILEAGE = "Mileage";
     private static final String CSV_PRICE = "Price";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String ENV_CAR = "CAR";
+    private static final String ENV_MODEL = "MODEL";
+    private static final String ENV_GENERATION = "GENERATION";
+    private static final String ENV_YEAR_MIN = "YEAR_MIN";
+    private static final String ENV_YEAR_MAX = "YEAR_MAX";
+    private static final String ENV_MIN_MILEAGE = "MIN_MILEAGE";
+    private static final String ENV_MAX_MILEAGE = "MAX_MILEAGE";
 
     public Car(CSVRecord record) {
         this(
@@ -57,5 +66,20 @@ public record Car(String link, String name, String model, String generation,
 
     private static String nullToEmpty(Object value) {
         return value == null ? EMPTY : value.toString();
+    }
+
+    public boolean isValid() {
+        String name = dotenv.get(ENV_CAR);
+        String model = dotenv.get(ENV_MODEL);
+        String generation = dotenv.get(ENV_GENERATION);
+        int minYear = Integer.parseInt(dotenv.get(ENV_YEAR_MIN));
+        int maxYear = Integer.parseInt(dotenv.get(ENV_YEAR_MAX));
+        int minMileage = Integer.parseInt(dotenv.get(ENV_MIN_MILEAGE));
+        int maxMileage = Integer.parseInt(dotenv.get(ENV_MAX_MILEAGE));
+        return this.name != null && this.name.equals(name) &&
+                this.model != null && this.model.equals(model) &&
+                this.generation != null && this.generation.equals(generation) &&
+                this.year != null && this.year >= minYear && this.year <= maxYear &&
+                this.mileage != null && this.mileage >= minMileage && this.mileage <= maxMileage;
     }
 }
--- src/main/java/org/example/parser/CarCsvProcessor.java
+++ src/main/java/org/example/parser/CarCsvProcessor.java
@@ -15,17 +15,9 @@ import java.util.List;
 import org.slf4j.Logger;
 
 public class CarCsvProcessor {
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String ENV_CAR = "CAR";
-    private static final String ENV_MODEL = "MODEL";
-    private static final String ENV_GENERATION = "GENERATION";
-    private static final String ENV_YEAR_MIN = "YEAR_MIN";
-    private static final String ENV_YEAR_MAX = "YEAR_MAX";
-    private static final String ENV_MIN_MILEAGE = "MIN_MILEAGE";
-    private static final String ENV_MAX_MILEAGE = "MAX_MILEAGE";
-    private static final String NOT_NUMBER_REGEX = "[^0-9]";
     private static final String SPACE_REMOVER = "";
     private static final Logger logger = LoggerFactory.getLogger(CarCsvProcessor.class);
+    private static final String NOT_NUMBER_REGEX = "[^0-9]";
 
     public List<Car> readFile(String filename) throws IOException {
         Reader csv = new FileReader(filename);
@@ -62,22 +54,9 @@ public class CarCsvProcessor {
     }
 
     public List<Car> filterCars(List<Car> carsToBeFiltered) {
-        String name = dotenv.get(ENV_CAR);
-        String model = dotenv.get(ENV_MODEL);
-        String generation = dotenv.get(ENV_GENERATION);
-        int minYear = Integer.parseInt(dotenv.get(ENV_YEAR_MIN));
-        int maxYear = Integer.parseInt(dotenv.get(ENV_YEAR_MAX));
-        int minMileage = Integer.parseInt(dotenv.get(ENV_MIN_MILEAGE));
-        int maxMileage = Integer.parseInt(dotenv.get(ENV_MAX_MILEAGE));
-
         return carsToBeFiltered
                 .stream()
-                .filter(car ->
-                        car.name() != null && car.name().equals(name) &&
-                                car.model() != null && car.model().equals(model) &&
-                                car.generation() != null && car.generation().equals(generation) &&
-                                car.year() != null && car.year() >= minYear && car.year() <= maxYear &&
-                                car.mileage() != null && car.mileage() >= minMileage && car.mileage() <= maxMileage
+                .filter(car -> car.isValid()
                 )
                 .toList();
     }
